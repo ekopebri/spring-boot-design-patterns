0916e74c90ab705ba84be4e3af2b8d453119590b
Eko Pebrisulistiyo <eko.pebrisulistiyo@gmail.com>
2021-05-22 20:45:22 +0800

    Initial Apps

---
+++ src/test/java/com/keteban/spring/pattern/SpringDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keteban.spring.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
